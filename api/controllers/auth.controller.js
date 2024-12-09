ec2dac096dfb8cf802667abc883f48ee36c36a88
SSY199 <s.s.y.9321423@gmail.com>
2024-12-09 09:38:12 +0530

    add admin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -87,6 +87,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign(
       {
         id: validUser._id,
+        isAdmin: validUser.isAdmin,
       },
       process.env.JWT_SECRET
     );
@@ -109,7 +110,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -132,7 +133,7 @@ export const google = async (req, res, next) => {
       });
       console.log(newUser);
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,11 @@ const userSchema = new mongoose.Schema({
   },
   profilePicture: {
     type: String,
-    default: "https://static.vecteezy.com/system/resources/thumbnails/002/387/693/small_2x/user-profile-icon-free-vector.jpg",
+    default: "https://files.oaiusercontent.com/file-1C6r4vaazQ5nymhsVN3oiE?se=2024-12-09T04%3A03%3A40Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D810ac378-9ae5-47ba-a36a-edea1c9bad6a.webp&sig=ia8SDcvL6tSZvWuTyXJCzF4GwnJX9nj6i9Vl6TUuRvk%3D",
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
   },
 },
 {timestamps: true}
